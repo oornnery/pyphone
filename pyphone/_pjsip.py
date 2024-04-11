4ba93ec5657ca275c09cebd09efa1dad6734a099
Fabio Souza <fabiohcsouza@outlook.com.br>
2024-04-11 17:27:24 -0300

    pjsip cli

--- pyphone/_pjsip.py
+++ pyphone/_pjsip.py
@@ -1,31 +1,30 @@
-# $Id: call.py 2171 2008-07-24 09:01:33Z bennylp $
-#
-# SIP call sample.
-#
-# Copyright (C) 2003-2008 Benny Prijono <benny@prijono.org>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
-#
+#!/usr/bin/env python3
 import sys
-import socket
 import re
-import netifaces
 import pjsua as pj
+
+import dotenv
+
 from rich.console import Console
 from rich.panel import Panel
+from rich.prompt import Prompt
 from rich import print
+
+
+# Create output console
+console = Console()
+# Load environment variables
+env = dotenv.dotenv_values(".env")
+
+
+LOG_LEVEL= 3
+current_call = None
+transport = None
+acc = None
+lib = None
+
+
+
 
 # Subclass to extend the Account and get notifications etc.
 
@@ -76,15 +75,6 @@ class MyCallCallback(pj.CallCallback):
             print("Call media state changed")
 
 
-console = Console()
-
-LOG_LEVEL= 3
-current_call = None
-transport = None
-acc = None
-lib = None
-
-
 # Logging callback
 def log_cb(level, str, len):
     console.print(
@@ -95,16 +85,95 @@ def log_cb(level, str, len):
         )
     )
 
+
+def start_service(username: str, password: str, domain: str, port: int = 5060):
+    global transport
+    global acc
+    global lib
+    try:
+        # Create library instance
+        lib = pj.Lib()
+        # Init library with default config and some customized
+        # logging config.
+        lib.init(
+            log_cfg=pj.LogConfig(level=LOG_LEVEL, callback=log_cb)
+            )
+        # Create UDP transport which listens to any available port
+        transport = lib.create_transport(
+            pj.TransportType.UDP, 
+            pj.TransportConfig(int(port))
+            )
+        # Set codec priorities
+        lib.set_codec_priority('PCMA/8000/1', 0)
+        lib.set_codec_priority('PCMU/8000/1', 32)
+        # Start the library
+        lib.start()
+        # Create local account
+        acc = lib.create_account(
+            pj.AccountConfig(
+                domain=domain,
+                username=username,
+                password=password
+            ),
+                cb=MyAccountCallback()
+            )
+        # Set presence to available
+        acc.set_presence_status(True, activity=pj.PresenceActivity.UNKNOWN)
+    
+    except pj.Error as e:
+        console.print(Panel("Exception: " + str(e), title="Error", border_style="red"))
+        quit()
+
+def call(uri: str):
+    """Send call"""
+    global current_call
+    global transport
+    global acc
+    global lib
+    
+    if not acc:
+        console.print(Panel("No Account SIP", title="Error", border_style="red"))
+        return
+    
+    try:
+        lck = lib.auto_lock()
+        call_uri = re.sub(r'^<sip:([a-zA-Z0-9]+)@(.*)>$', rf'<sip:{uri}@\2>', acc.info().uri)
+        print("Making call to", call_uri)
+        current_call = acc.make_call(
+            call_uri,
+            cb=MyCallCallback(),
+            )
+    except pj.Error as e:
+        console.print(Panel("Exception: " + str(e), title="Error", border_style="red"))
+        current_call = None
+    finally:
+        del lck
+
+def hangup(code=487):
+    """Call hangup"""
+    global current_call
+    global transport
+    global acc
+    global lib
+    if not current_call:
+        console.print(Panel("No call", title="Error", border_style="red"))
+        return
+    try:
+        current_call.hangup(code)
+    except pj.Error as e:
+        console.print(Panel("Exception: " + str(e), title="Error", border_style="red"))
+    finally:
+        current_call = None
+
 def quit():
     global current_call
     global transport
     global acc
     global lib
+    console.print(Panel("Quitting", title="Status", border_style="green"))
     if current_call:
-        current_call.hangup(487)
-        current_call = None
-    if transport:
-        transport = None
+        hangup()
+    transport = None
     if acc:
         acc.delete()
         acc = None
@@ -114,128 +183,130 @@ def quit():
         lib = None
     sys.exit(0)
 
+def account_info():
+    """Print account info"""
+    global acc
+    if not acc:
+        console.print(Panel("No Account SIP", title="Error", border_style="red"))
+        return
 
-try:
-    # Create library instance
-    lib = pj.Lib()
-    # Init library with default config and some customized
-    # logging config.
-    lib.init(
-        log_cfg=pj.LogConfig(level=LOG_LEVEL, callback=log_cb)
+    ua = acc.info()
+    data=[
+        ('URI', ua.uri),
+        ('Reg Status', ua.reg_status),
+        ('Reg Reason', ua.reg_reason),
+        ('Reg Active', ua.reg_active),
+        ('Reg Expires', ua.reg_expires),
+        ('Online Status', ua.online_status),
+        ('Online Text', ua.online_text),
+    ]
+    console.print(
+        Panel(
+            ''.join([f'[bold]{i[0]}[/bold]: {i[1]}\n' for i in data]),
+            title="Status",
+            border_style="green",
         )
+    )
 
-    # Create UDP transport which listens to any available port
-    transport = lib.create_transport(
-        pj.TransportType.UDP, 
-        pj.TransportConfig(0)
-        )
-    # Start the library
-    lib.start()
-
-    lib.set_codec_priority('PCMA/8000/1', 0)
-    lib.set_codec_priority('PCMU/8000/1', 32)
+def current_call_info():
+    """Print current call info"""
+    global current_call
     
-    # Create local account
-    acc = lib.create_account(pj.AccountConfig(
-        "proxy2.idtbrasilhosted.com", 
-        "062099137", 
-        "fabio.2626",
-        ),
-        cb=MyAccountCallback()
+    if not current_call:
+        console.print(Panel("No call", title="Error", border_style="red"))
+        return
+    
+    ci = current_call.info()
+    data = [
+        ('Role', ci.role),
+        ('Account', ci.account),
+        ('URI', ci.uri),
+        ('Contact', ci.contact),
+        ('Remote uri', ci.remote_uri),
+        ('Remote contact', ci.remote_contact),
+        ('SIP Call Id', ci.sip_call_id),
+        ('State', ci.state),
+        ('State text', ci.state_text),
+        ('Last code', ci.last_code),
+        ('Last reason', ci.last_reason),
+        ('Media state', ci.media_state),
+        ('Media dir', ci.media_dir),
+        ('Conf slot', ci.conf_slot),
+        ('Call time', ci.call_time),
+        ('Total time', ci.total_time),
+    ]
+    console.print(
+        Panel(
+            ''.join([f'[bold]{i[0]}[/bold]: {i[1]}\n' for i in data]),
+            title="Call Info",
+            border_style="green",
         )
-    # Set presence to available
-    acc.set_presence_status(True, activity=pj.PresenceActivity.UNKNOWN)
-    # Loop
-    print("\nEnter SIP URI or 'q' to quit")
-    while True:
-        uri = input('>> ')
-        # quit
-        if uri == "q":
-            break
-        # make call
-        if uri == 'c':
-            if current_call:
-                current_call.hangup(487)
-                current_call = None
-            print("Enter SIP URI to call")
-            call_uri = input('>> ')
-            if call_uri == "":
-                continue
-            lck = lib.auto_lock()
-            try:
-                print("Making call to", call_uri)
-                current_call = acc.make_call(
-                    re.sub(r'^<sip:([a-zA-Z0-9]+)@(.*)>$', rf'<sip:{call_uri}@\2>', acc.info().uri),
-                    )
-                
-                # while True:
-                #     print(current_call.info().state_text)
-                #     if current_call.info().state == pj.CallState.DISCONNECTED:
-                #         break
-                
-            except pj.Error as e:
-                current_call = None
-                print("Exception: " + str(e))
-            del lck
-        # hangup
-        if uri == 'h':
-            if current_call:
-                current_call.hangup(487)
-                current_call = None
-            continue
-        # status
-        if uri == 's':
-            print('Status: \n')
-            data=[
-                ('URI', acc.info().uri),
-                ('Reg Status', acc.info().reg_status),
-                ('Reg Reason', acc.info().reg_reason),
-                ('Reg Active', acc.info().reg_active),
-                ('Reg Expires', acc.info().reg_expires),
-                ('Online Status', acc.info().online_status),
-                ('Online Text', acc.info().online_text),
-            ]
-            console.print(
-                Panel(
-                    ''.join([f'[bold]{i[0]}[/bold]: {i[1]}\n' for i in data]),
-                    title="Status",
-                    border_style="green",
-                )
-            )
-        if uri == 'ci':
-            if current_call:
-                data = [
-                    ('Role', current_call.info().role),
-                    ('Account', current_call.info().account),
-                    ('URI', current_call.info().uri),
-                    ('Contact', current_call.info().contact),
-                    ('Remote uri', current_call.info().remote_uri),
-                    ('Remote contact', current_call.info().remote_contact),
-                    ('SIP Call Id', current_call.info().sip_call_id),
-                    ('State', current_call.info().state),
-                    ('State text', current_call.info().state_text),
-                    ('Last code', current_call.info().last_code),
-                    ('Last reason', current_call.info().last_reason),
-                    ('Media state', current_call.info().media_state),
-                    ('Media dir', current_call.info().media_dir),
-                    ('Conf slot', current_call.info().conf_slot),
-                    ('Call time', current_call.info().call_time),
-                    ('Total time', current_call.info().total_time),
-                ]
-                console.print(
-                    Panel(
-                        ''.join([f'[bold]{i[0]}[/bold]: {i[1]}\n' for i in data]),
-                        title="Call Info",
-                        border_style="green",
-                    )
-                )
-except pj.Error as e:
-    print("Exception: " + str(e))
-    quit()
-except KeyboardInterrupt:
-    console.print("\nInterrupted")
-    quit()
-finally:
-    console.print("\nInterrupted")
-    quit()
+    )
+
+
+def main(username: str, password: str, domain: str, port: int = 5060):
+    global current_call
+    global acc
+    options = [
+        'c - call',
+        'h - hangup',
+        's - status',
+        'ci - call info',
+        'a - Help', 
+        'q - quit',
+    ]
+    def menu():
+        console.print(Panel('\n'.join(options), title="Commands", border_style="green"))
+    
+    try:
+        console.print(Panel("Starting", title="Status", border_style="green"))
+        console.print(Panel(f"{username}\n{password}\n{domain}\n{port}", title="Status", border_style="green"))
         
+        start_service(username, password, domain, port)
+        # Loop
+        
+        while True:
+            console.clear()
+            menu()
+            ask = Prompt.ask('>> ', choices=[x.split('-')[0].strip() for x in options], default='a')
+            # quit
+            if ask == "q":
+                break
+            if ask == "a":
+                menu()
+            # make call
+            if ask == 'c':
+                if current_call:
+                    console.print(Panel("Call in progress", title="Error", border_style="red"))
+                    continue
+                call_uri = Prompt.ask("Call URI: ")
+                if call_uri == "":
+                    continue
+                call(call_uri)
+            # hangup
+            if ask == 'h':
+                hangup()
+            # status
+            if ask == 's':
+                account_info()
+            if ask == 'ci':
+                current_call_info()
+            input("\nPress ENTER to continue...")
+    except pj.Error as e:
+        console.print(Panel("Exception: " + str(e), title="Error", border_style="red"))
+        quit()
+    except KeyboardInterrupt:
+        console.print("\nInterrupted")
+        quit()
+    # finally:
+    #     console.print("\nInterrupted")
+    #     quit()
+        
+
+if __name__ == "__main__":
+    main(
+        username=env.get('USERNAME'),
+        password=env.get('PASSWORD'),
+        domain=env.get('DOMAIN'),
+        port=env.get('PORT', 5060),
+    )
