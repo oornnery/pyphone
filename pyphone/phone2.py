d71269a46315620df536f4c1751cfb25cdd6b85a
Fabio Souza <fabiohcsouza@outlook.com.br>
2024-04-24 21:28:20 -0300

    tests with pjsua2

---
+++ pyphone/phone2.py
@@ -0,0 +1,101 @@
+import pjsua2 as pj
+import time
+import dotenv
+
+env = dotenv.dotenv_values(".env")
+
+class VoIPManager:
+    def __init__(self):
+        # Create and initialize the library
+        ep_cfg = pj.EpConfig()
+        self.ep = pj.Endpoint()
+        self.ep.libCreate()
+        self.ep.libInit(ep_cfg)
+        # Create SIP transport. Error handling sample is shown
+        sip_transport_config = pj.TransportConfig()
+        sip_transport_config.port = 5060
+        self.transport = self.ep.transportCreate(
+            pj.PJSIP_TRANSPORT_UDP, 
+            sip_transport_config
+            )
+        # start the library
+        self.ep.libStart()
+        # SIP account
+        self.acc = pj.Account()
+        self.prm = pj.CallOpParam()
+        self.call = pj.Call(self.acc)
+
+    def register(self, username, password, domain):
+        acc_cfg = pj.AccountConfig()
+        acc_cfg.idUri = f"sip:{username}@{domain}"
+        acc_cfg.regConfig.registrarUri = f"sip:{domain}"
+        cred = pj.AuthCredInfo("digest", "*", username, 0, password)
+        acc_cfg.sipConfig.authCreds.append(cred)
+        # Create the account
+        self.acc.create(acc_cfg)
+    
+    def set_presence_status(self):
+        status = pj.PresenceStatus()
+        status.status = pj.PJSUA_BUDDY_STATUS_ONLINE
+        self.acc.setOnlineStatus(status)
+
+    def unregister(self):
+        self.acc.shutdown()
+
+    def account_info(self):
+        return self.acc.getInfo()
+    
+    def make_call(self, destination):
+        dest_uri = f"sip:{destination}@{env.get('DOMAIN')}"
+        return self.call.makeCall(dest_uri, self.prm)
+
+    def hangup(self):
+        self.prm.statusCode = pj.PJSIP_SC_REQUEST_TERMINATED
+        self.call.hangup(prm)
+        self.call.hasMedia
+
+    def set_callback(self, callback_function):
+        # Aqui você pode definir um callback para eventos específicos, como chamadas recebidas
+        # Isso pode ser feito sobrescrevendo os métodos de callback na classe CallCallback
+        pass
+    
+    def call_info(self):
+        return self.call.getInfo()
+    
+    def quit(self):
+        if self.call.getInfo().state != pj.PJSIP_INV_STATE_DISCONNECTED:
+            self.hangup()
+        # if self.acc.calls.count > 0:
+        #     for call in self.acc.calls:
+        #         call.hangup()
+        if self.acc:
+            self.unregister()
+        self.ep.libDestroy()
+        sys.exit(0)
+    
+# Exemplo de uso
+if __name__ == "__main__":
+    voip_manager = VoIPManager()
+    voip_manager.register(env.get('USERNAME'), env.get('PASSWORD'), env.get('DOMAIN'))
+    print(voip_manager.acc.getId())
+    print(voip_manager.acc.getInfo().id)
+    print(voip_manager.acc.getInfo().uri)
+    print(voip_manager.acc.getInfo().regIsConfigured)
+    print(voip_manager.acc.getInfo().regIsActive)
+    print(voip_manager.acc.getInfo().regExpiresSec)
+    print(voip_manager.acc.getInfo().regStatus)
+    print(voip_manager.acc.getInfo().regStatusText)
+    print(voip_manager.acc.getInfo().regLastErr)
+    print(voip_manager.acc.getInfo().onlineStatus)
+    print(voip_manager.acc.getInfo().onlineStatusText)
+    
+    input("Pressione Enter para fazer uma chamada...")
+    voip_manager.make_call(env.get('DESTINATION'))
+    time.sleep(10)
+    
+    input("Pressione Enter para encerrar a chamada...")
+    voip_manager.hangup()
+    
+    input("Pressione Enter para sair...")
+    
+    voip_manager.quit()
